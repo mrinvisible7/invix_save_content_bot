81a9629e7be0979c184146fb66aa2893c7fd397e
Mr. Invisible <122687482+mrinvisible7@users.noreply.github.com>
2023-04-18 12:40:59 +0530

    Update progress.py

--- main/plugins/progress.py
+++ main/plugins/progress.py
@@ -36,7 +36,7 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "**[{0}{1}]**".format(
+        progress = "**[{0}{1}]**" \n .format(
             ''.join(                
                     FINISHED_PROGRESS_STR
                     for _ in range(math.floor(percentage / 10))                
@@ -47,7 +47,7 @@ async def progress_for_pyrogram(
             ),
         )  
 
-        tmp = progress + \n "GROSSS: {0} of {1}\n\nSpeed: {2}/s\n\nETA: {3}\n".format(
+        tmp = progress + "GROSSS: {0} of {1}\n\nSpeed: {2}/s\n\nETA: {3}\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
